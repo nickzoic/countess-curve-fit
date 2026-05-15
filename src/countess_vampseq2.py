758b2e08265edec15a2ff80ebbc37f3da09bc847
Nick Moore <nick@zoic.org>
2026-05-15 13:28:22 +1000

    add in sigmas: this should probably turn into a generic curve fitting plugin because it's 90% of the way there already

--- src/countess_vampseq2.py
+++ src/countess_vampseq2.py
@@ -1,5 +1,5 @@
 """CountESS VAMPseq2 Plugin"""
-
+import logging
 from typing import Iterable, Optional, Any, Dict
 
 from countess.core.plugins import DuckdbTransformPlugin
@@ -9,22 +9,26 @@ from countess.core.parameters import (
     PerColumnArrayParam,
     PerNumericColumnArrayParam,
     TabularMultiParam,
-    ColumnGroupChoiceParam
+    ColumnGroupChoiceParam,
+    ColumnGroupOrNoneChoiceParam,
 )
 from scipy.stats import norm
 from scipy.optimize import curve_fit
 
 VERSION = "0.0.1"
 
+logger = logging.getLogger(__name__)
+
 def func(x, mu, sigma):
     return norm.cdf(x+1, loc=mu, scale=sigma) - norm.cdf(x, loc=mu, scale=sigma)
 
-def fit(counts):
+def fit(counts: list[float], sigmas: Optional[list[float]] = None) -> tuple[float, float, float]:
     total = sum(counts)
     popt, pcov, *_ = curve_fit(
         func,
-        xdata = list(range(0, len(counts)-1)),
-        ydata = [ c / total for c in counts[0:-1] ]
+        xdata = list(range(0, len(counts))),
+        ydata = [ c / total for c in counts ],
+        sigma = [ s / total for s in sigmas ] if sigmas else None,
     )
     return popt[0], popt[1], pcov[0][0]
 
@@ -35,7 +39,8 @@ class VampSeq2Plugin(DuckdbTransformPlugin):
     description = "VAMP-seq Scoring (Enhanced)"
     version = VERSION
 
-    columns = ColumnGroupChoiceParam("Count Columns")
+    columns = ColumnGroupChoiceParam("Frequency Columns")
+    sigmas = ColumnGroupOrNoneChoiceParam("Sigma Columns")
 
     def add_fields(self):
         return {
@@ -45,7 +50,9 @@ class VampSeq2Plugin(DuckdbTransformPlugin):
         }
 
     def transform(self, data: dict[str, Any]) -> Optional[Dict[str, Any]]:
+        logger.debug(data)
         prefix = self.columns.get_column_prefix()
         counts = [ data[x] for x in data if x.startswith(prefix) ]
+        sigmas = [ data[x] for x in data if x.startswith(self.sigmas.get_column_prefix()) ] if self.sigmas.is_not_none() else None
         data["mu"], data["sigma"], data["var_mu"] = fit(counts)
         return data
